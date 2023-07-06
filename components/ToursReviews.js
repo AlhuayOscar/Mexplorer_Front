6ed90948712d0fa82fafb9fe1e9f09a450afab4a
eudesmieres <eumieres@gmail.com>
2023-07-06 19:46:51 -0300

    detalles reviews

--- components/ToursReviews.js
+++ components/ToursReviews.js
@@ -19,9 +19,38 @@ margin-top: 5px;
 
 const ColsWrapper = styled.div`
 display: grid;
-grid-template-columns: 1fr 1fr;
-gap: 40px;
-`
+grid-template-columns: 1fr;
+gap: 20px;
+@media screen and (min-width: 768px) {
+    grid-template-columns: 1fr 1fr;
+    gap: 40px;
+}
+`;
+
+const ReviewWrapper = styled.div`
+margin-bottom: 10px;
+border-top: 1px solid #ccc;
+padding: 10px 0;
+
+h3{
+    margin:10px;
+    font-size:1rem;
+    color: #444;
+}
+p{
+    margin:0;
+    color: #555;
+}
+`;
+
+const ReviewHeader = styled.div`
+display: flex;
+justify-content: space-between;
+time{
+    font-size: 14px;
+    color: #aaa;
+}
+`;
 
 export default function ToursReviews({ tour }) {
     const [title, setTitle] = useState('');
@@ -35,51 +64,61 @@ export default function ToursReviews({ tour }) {
             setTitle('');
             setDescription('');
             setStars(0);
-            alert('ok');
+            loadReviews();
         });
     }
 
     useEffect(() => {
+        loadReviews();
+    }, []);
+
+    function loadReviews() {
         axios.get('/api/reviews?tour=' + tour._id).then(res => {
             setReviews(res.data);
         });
-    }, []);
+    }
     return (
         <div>
             <Title>Reseña</Title>
             <ColsWrapper>
-                <WhiteBox>
-                    <Subtitle>Agrega una reseña</Subtitle>
-                    <div>
-                        <StarsRanting onChange={setStars} />
-                    </div>
-                    <Input
-                        value={title}
-                        onChange={ev => setTitle(ev.target.value)}
-                        placeholder="Titulo" />
-                    <Textarea
-                        value={description}
-                        onChange={ev => setDescription(ev.target.value)}
-                        placeholder="Deja tu opinión" />
-                    <div>
-                        <Button onClick={submitReview} >Enviar</Button>
-                    </div>
-                </WhiteBox>
-                <WhiteBox>
-                    <Subtitle>Reseña</Subtitle>
-                    {reviews.length === 0 ? (
-                        <p>No tiene reseña</p>
-                    ) : (
-                        reviews.map(review => (
-                            <div key={review.id}>
-                                <StarsRanting size={'sm'} disabled={true} defaulHowMany={review.stars} />
-                                <time>{(new Date(review.createdAt)).toLocaleString('sv-SE')}</time>
-                                {review.title}
-                                {review.description}
-                            </div>
-                        ))
-                    )}
-                </WhiteBox>
+                <div>
+                    <WhiteBox>
+                        <Subtitle>Agrega una reseña</Subtitle>
+                        <div>
+                            <StarsRanting onChange={setStars} />
+                        </div>
+                        <Input
+                            value={title}
+                            onChange={ev => setTitle(ev.target.value)}
+                            placeholder="Titulo" />
+                        <Textarea
+                            value={description}
+                            onChange={ev => setDescription(ev.target.value)}
+                            placeholder="Deja tu opinión" />
+                        <div>
+                            <Button onClick={submitReview} >Enviar</Button>
+                        </div>
+                    </WhiteBox>
+                </div>
+                <div>
+                    <WhiteBox>
+                        <Subtitle>Reseña</Subtitle>
+                        {reviews.length === 0 ? (
+                            <p>No tiene reseña</p>
+                        ) : (
+                            reviews.map(review => (
+                                <ReviewWrapper key={review.id}>
+                                    <ReviewHeader>
+                                        <StarsRanting size={'sm'} disabled={true} defaulHowMany={review.stars} />
+                                        <time>{(new Date(review.createdAt)).toLocaleString('sv-SE')}</time>
+                                    </ReviewHeader>
+                                    <h3>{review.title}</h3>
+                                    <p>{review.description}</p>
+                                </ReviewWrapper>
+                            ))
+                        )}
+                    </WhiteBox>
+                </div>
             </ColsWrapper>
         </div>
     );
