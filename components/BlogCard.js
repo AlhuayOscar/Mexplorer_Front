6703544af5f9566fc9cedc66b47d588d2a9a1e48
AlhuayOscar <90626121+AlhuayOscar@users.noreply.github.com>
2023-07-16 11:33:37 -0300

    Blog funcional, oscar

---
+++ components/BlogCard.js
@@ -0,0 +1,91 @@
+import React from "react";
+import styled from "styled-components";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+
+const CardContainer = styled.div`
+  background-color: #f5f5f5;
+  border-radius: 8px;
+  padding: 8px;
+
+  width: 350px;
+  height: 465px;
+`;
+
+const Title = styled.h2`
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 8px;
+  color: #103f54;
+  text-align: center;
+`;
+
+const Subtitle = styled.h3`
+  font-size: 18px;
+  margin-bottom: 8px;
+  text-align: center;
+`;
+
+const Description = styled.p`
+  font-size: 16px;
+  padding-inline: 20px;
+`;
+const BlogDate = styled.span`
+  text-align: start;
+
+  p {
+    padding-inline: 20px;
+    display: flex;
+    align-items: center;
+    gap: 3px;
+  }
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 200px;
+  max-height: 200px;
+  object-fit: cover;
+  filter: blur(
+    0.4px
+  ); /* Puedes ajustar el valor de blur según tus preferencias */
+`;
+
+const BlogCard = ({ blog }) => {
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const day = date.getDate().toString().padStart(2, "0");
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    const year = date.getFullYear().toString().slice(-2);
+    return `${day}-${month}-${year}`;
+  };
+
+  const truncateDescription = (description, maxLength) => {
+    if (description.length <= maxLength) {
+      return description;
+    } else {
+      const truncated = description.substring(0, maxLength);
+      const lastSpaceIndex = truncated.lastIndexOf(" ");
+      return truncated.substring(0, lastSpaceIndex) + "...";
+    }
+  };
+
+  const truncatedDescription = truncateDescription(blog.description, 160);
+
+  return (
+    <CardContainer>
+      <Image src={blog.images[0]} alt="Blog Image" />
+      <Title>{blog.title}</Title>
+      <BlogDate>
+        <p>
+          <CalendarMonthIcon />
+          {formatDate(blog.date)}
+        </p>
+      </BlogDate>
+      <Subtitle>{blog.subtitle}</Subtitle>
+      <Description>{truncatedDescription}</Description>
+      {/* Otras propiedades del blog que desees mostrar */}
+    </CardContainer>
+  );
+};
+
+export default BlogCard;
--- pages/blog.js
+++ pages/blog.js
@@ -1,40 +1,162 @@
-import React from "react";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import BlogCards from "@/components/BlogCards";
+import Header from "@/components/Header";
+import styled, { keyframes } from "styled-components";
+import Footer from "@/components/Footer";
 
-const BlogComponent = () => {
+const SearchContainer = styled.div`
+  display: flex;
+  padding-block: 20px;
+  justify-content: space-around;
+`;
+const SearchInput = styled.input`
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  outline: none;
+  transition: 0.1s ease;
+  &:focus {
+    transition: 0.1s ease;
+    border-color: #888;
+    box-shadow: 0 0 5px #888;
+  }
+  &:hover {
+    transform: scale(1.05);
+    transition: 0.1s ease;
+  }
+`;
+const bounceAnimation = keyframes`
+  0%{
+     transform: scale(1);
+    transition: 0.2s ease;
+  }
+  50% {
+     transform: scale(1.1);
+    transition: 0.2s ease;
+  }
+  100% {
+     transform: scale(1);
+    transition: 0.2s ease;
+  }
+`;
+
+const CleanSearch = styled.button`
+  background-color: #fff;
+  color: #1a1a1a;
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  outline: none;
+  transition: 0.2s ease;
+  &:hover {
+    transition: 0.2s ease;
+    border-color: #888;
+    box-shadow: 0 0 5px #888;
+  }
+  &:hover {
+    cursor: pointer;
+    transform: scale(1.1);
+    transition: 0.1s ease;
+  }
+  &:active {
+    animation: ${bounceAnimation} 0.6s;
+  }
+`;
+const Blog = () => {
   const [blogs, setBlogs] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredBlogs, setFilteredBlogs] = useState([]);
+  const [recentSearches, setRecentSearches] = useState([]);
 
   useEffect(() => {
     fetchBlogs();
+    loadRecentSearches();
   }, []);
 
   const fetchBlogs = async () => {
     try {
-      const response = await axios.get("/api/blogs"); // Cambia la ruta "/api/blogs" por la ruta correcta de tu API para obtener los blogs
+      const response = await axios.get("/api/blogs"); // Reemplaza con la ruta correcta de tu API
       setBlogs(response.data);
     } catch (error) {
       console.error(error);
     }
   };
 
+  const loadRecentSearches = () => {
+    const searches = localStorage.getItem("recentSearches");
+    if (searches) {
+      setRecentSearches(JSON.parse(searches));
+    }
+  };
+
+  const saveRecentSearches = () => {
+    localStorage.setItem("recentSearches", JSON.stringify(recentSearches));
+  };
+
+  const handleSearch = (event) => {
+    const term = event.target.value;
+    setSearchTerm(term);
+  };
+
+  const handleEnterKeyPress = (event) => {
+    if (event.key === "Enter") {
+      filterBlogs(searchTerm);
+      updateRecentSearches(searchTerm);
+    }
+  };
+
+  const filterBlogs = (term) => {
+    const filtered = blogs.filter((blog) => {
+      const blogTitle = blog.title.toLowerCase();
+      return blogTitle.includes(term.toLowerCase());
+    });
+    setFilteredBlogs(filtered);
+  };
+
+  const updateRecentSearches = (term) => {
+    if (term && !recentSearches.includes(term)) {
+      const updatedSearches = [...recentSearches, term];
+      setRecentSearches(updatedSearches.slice(-5)); // Keep the last 5 searches
+      saveRecentSearches();
+    }
+  };
+
+  const clearRecentSearches = () => {
+    setRecentSearches([]);
+    localStorage.removeItem("recentSearches");
+  };
+
   return (
-    <div>
-      <h1>Lista de Blogs</h1>
-      {blogs.map((blog) => (
-        <div key={blog._id}>
-          <h2>{blog.title}</h2>
-          <h3>{blog.subtitle}</h3>
-          <p>{blog.description}</p>
-          {blog.images.map((image, index) => (
-            <img key={index} src={image} alt={`Imagen ${index + 1}`} />
+    <>
+      <Header />
+      <SearchContainer>
+        <div>
+          {recentSearches.map((search, index) => (
+            <span key={index} style={{ marginRight: "5px" }}>
+              {search.split(" ")[0]}
+            </span>
           ))}
-          <p>{blog.date}</p>
-          <p>{blog.location}</p>
         </div>
-      ))}
-    </div>
+        <SearchInput
+          type="text"
+          placeholder="Buscar..."
+          value={searchTerm}
+          onChange={handleSearch}
+          onKeyPress={handleEnterKeyPress}
+        />
+        <CleanSearch onClick={clearRecentSearches}>
+          Limpiar búsquedas recientes
+        </CleanSearch>
+      </SearchContainer>
+      {searchTerm !== "" ? (
+        <BlogCards blogs={filteredBlogs} />
+      ) : (
+        <BlogCards blogs={blogs} />
+      )}
+      <Footer />
+    </>
   );
 };
 
-export default BlogComponent;
+export default Blog;
