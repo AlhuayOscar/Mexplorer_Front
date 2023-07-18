3bceae19813baf8b6aa7f19e753717d9f3dca0ab
AlhuayOscar <90626121+AlhuayOscar@users.noreply.github.com>
2023-07-18 01:05:15 -0300

    Console errors Fixed, Es qué es una versión anterior de las librerias para que funcione como queremos, oscar

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,13 @@
-
-import { I18nextProvider } from 'react-i18next';
-import i18n from '@/i18n'; // Importa el archivo i18n.js que configuramos
+import { I18nextProvider } from "react-i18next";
+import i18n from "@/i18n";
 import { createGlobalStyle } from "styled-components";
 import { CartContextProvider } from "@/components/CartContext";
 
 const GlobalStyles = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap');
-  body{
+  body {
     background-color: #eee;
-    padding:0;
-    margin:0;
+    padding: 0;
+    margin: 0;
     font-family: 'Poppins', sans-serif;
   }
 `;
@@ -17,8 +15,8 @@ const GlobalStyles = createGlobalStyle`
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <GlobalStyles />
       <I18nextProvider i18n={i18n}>
-        <GlobalStyles />
         <CartContextProvider>
           <Component {...pageProps} />
         </CartContextProvider>
