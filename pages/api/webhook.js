d38ff7b70a245d429b3991ee765467e232892564
Alan Winkler <alanquimeywinkler@gmail.com>
2023-07-26 20:56:48 -0300

    pull developer

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -31,9 +31,6 @@ export default async function handler(req, res) {
   // Handle the event
   switch (event.type) {
     case "checkout.session.completed":
-      console.log(
-        "-----------------------------------------------------------"
-      );
       const data = event.data.object;
       const orderId = data.metadata.orderId;
       const paid = data.payment_status === "paid";
