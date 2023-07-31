f37dbecf052e8987db35ce9047a3802cda86522a
AlhuayOscar <90626121+AlhuayOscar@users.noreply.github.com>
2023-07-31 01:50:04 -0300

    Index Con animaciones, oscar

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled, { keyframes } from "styled-components";
 import Header from "@/components/Header";
 import Featured from "@/components/Featured";
@@ -6,6 +6,7 @@ import Footer from "@/components/Footer";
 import { Tour } from "@/models/Tour";
 import { mongooseConnect } from "@/lib/mongoose";
 import NewTours from "@/components/NewTours";
+import Spinner from "react-spinner";
 
 // Definimos una animación de deslizamiento hacia abajo
 const slideDownAnimation = keyframes`
@@ -20,27 +21,57 @@ const slideDownAnimation = keyframes`
 // Creamos un nuevo componente estilizado para la imagen
 const StyledImage = styled.img`
   position: absolute;
-  top: 0;
+  top: 50%;
   left: 50%;
-  transform: translateX(-50%);
+  transform: translate(-50%, -50%);
   width: auto;
-  height: 70px;
-  z-index: -1; /* Colocamos la imagen detrás del resto del contenido */
+  height: 120px;
+  z-index: 9999; /* Colocamos la imagen encima de todo */
 `;
 
 // Creamos un nuevo componente estilizado con la animación aplicada
 const AnimatedHeaderWrapper = styled.div`
   position: relative; /* Aseguramos que la posición sea relativa para que la imagen con posición absoluta esté contenida en este div */
-  animation: ${slideDownAnimation} 3.5s ease;
+  animation: ${slideDownAnimation} 8.5s ease;
+`;
+
+// Estilos para el contenedor de la pantalla de carga
+const LoadingContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 1); /* Color marrón/negro opaco */
+  z-index: 9998; /* Colocamos la pantalla de carga por encima del resto del contenido */
 `;
 
 export default function HomePage({ featuredTour, newTours, promoTours }) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulamos una carga de datos, por ejemplo, espera 3 segundos
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div>
+      {loading && (
+        <LoadingContainer>
+          <StyledImage src="/mex_logo.png" alt="Logo de México" fill="true" />
+          <Spinner color="#007BFF" />
+        </LoadingContainer>
+      )}
       <AnimatedHeaderWrapper>
         <Header />
       </AnimatedHeaderWrapper>
-      <StyledImage src="/mex_logo.png" alt="Logo de México" fill="true" />
       <Featured tour={featuredTour} />
       <NewTours tours={newTours} promo={promoTours} />
       <Footer />
