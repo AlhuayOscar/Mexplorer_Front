e5f75ca0f663577a0313ec119bbd4f88d4f8a25e
AlhuayOscar <90626121+AlhuayOscar@users.noreply.github.com>
2023-07-31 00:47:26 -0300

    Index Con animacion, oscar

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import React from "react";
+import styled, { keyframes } from "styled-components";
 import Header from "@/components/Header";
 import Featured from "@/components/Featured";
 import Footer from "@/components/Footer";
@@ -5,10 +7,40 @@ import { Tour } from "@/models/Tour";
 import { mongooseConnect } from "@/lib/mongoose";
 import NewTours from "@/components/NewTours";
 
+// Definimos una animación de deslizamiento hacia abajo
+const slideDownAnimation = keyframes`
+  0% {
+    transform: translateY(-100%);
+  }
+  100% {
+    transform: translateY(0);
+  }
+`;
+
+// Creamos un nuevo componente estilizado para la imagen
+const StyledImage = styled.img`
+  position: absolute;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  width: auto;
+  height: 70px;
+  z-index: -1; /* Colocamos la imagen detrás del resto del contenido */
+`;
+
+// Creamos un nuevo componente estilizado con la animación aplicada
+const AnimatedHeaderWrapper = styled.div`
+  position: relative; /* Aseguramos que la posición sea relativa para que la imagen con posición absoluta esté contenida en este div */
+  animation: ${slideDownAnimation} 3.5s ease;
+`;
+
 export default function HomePage({ featuredTour, newTours, promoTours }) {
   return (
     <div>
-      <Header />
+      <AnimatedHeaderWrapper>
+        <Header />
+      </AnimatedHeaderWrapper>
+      <StyledImage src="/mex_logo.png" alt="Logo de México" fill="true" />
       <Featured tour={featuredTour} />
       <NewTours tours={newTours} promo={promoTours} />
       <Footer />
