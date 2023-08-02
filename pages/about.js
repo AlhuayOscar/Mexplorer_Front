96c935856c5cfd2e04aedf5b145e6707209052bb
AlhuayOscar <90626121+AlhuayOscar@users.noreply.github.com>
2023-08-02 00:29:57 -0300

    Responsividad completa abotu us, oscar

--- pages/about.js
+++ pages/about.js
@@ -23,7 +23,7 @@ const ImageWrapper = styled.div`
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
-  max-width: 1500px;
+  max-width: 1400px;
 
   @media (max-width: 1100px) {
     flex-direction: column;
@@ -37,16 +37,47 @@ const ImageWrapper = styled.div`
 
 const StyledSpan = styled.span`
   font-size: 20px;
-  padding-inline: 5%;
+  padding-inline: 1%;
   h1 {
     font-size: 35px;
   }
   @media (max-width: 1100px) {
     padding-block: 5%;
   }
+  h3 span {
+    color: #ff5733; /* M */
+    &:nth-child(2) {
+      color: #daf7a6; /* e */
+    }
+    &:nth-child(3) {
+      color: #c70039; /* x */
+    }
+    &:nth-child(4) {
+      color: #7d3c98; /* p */
+    }
+    &:nth-child(5) {
+      color: #2ecc71; /* l */
+    }
+    &:nth-child(6) {
+      color: #ffc300; /* o */
+    }
+    &:nth-child(7) {
+      color: #3498db; /* r */
+    }
+    &:nth-child(8) {
+      color: #33ffd7; /* e */
+    }
+    &:nth-child(9) {
+      color: #f08080; /* r */
+    }
+    &:nth-child(10) {
+      color: #f0afaf; /* ! */
+    }
+  }
 `;
-const StyledSalute = styled.h1`
-  padding-inline: 5%;
+const StyledSalute = styled.h2`
+  font-weight: 600;
+  max-width: 1200px;
   @media (max-width: 1100px) {
     padding-block: 5%;
   }
@@ -57,6 +88,59 @@ const ResponsiveImage = styled.img`
   @media (max-width: 1100px) {
     width: 100%;
   }
+`;
+const StyledMexplorer = styled.span`
+  color: #ee2743; /* M */
+  font-size: 50px; /* Add the font size here */
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); /* Add the filter shadow here */
+`;
+
+const StyledE = styled.span`
+  color: #ed2286; /* e */
+  font-size: 50px; /* Add the font size here */
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.8); /* Add the filter shadow here */
+`;
+
+const StyledX = styled.span`
+  color: #eeb527; /* x */
+  font-size: 50px; /* Add the font size here */
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.8); /* Add the filter shadow here */
+`;
+
+const StyledP = styled.span`
+  color: #ac2484; /* p */
+  font-size: 50px; /* Add the font size here */
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.8); /* Add the filter shadow here */
+`;
+
+const StyledL = styled.span`
+  color: #84c441; /* l */
+  font-size: 50px; /* Add the font size here */
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.8); /* Add the filter shadow here */
+`;
+
+const StyledO = styled.span`
+  color: #00abbd; /* o */
+  font-size: 50px; /* Add the font size here */
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.8); /* Add the filter shadow here */
+`;
+
+const StyledR1 = styled.span`
+  color: #ee2743; /* r */
+  font-size: 50px; /* Add the font size here */
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.8); /* Add the filter shadow here */
+`;
+
+const StyledR2 = styled.span`
+  color: #ed2286; /* e */
+  font-size: 50px; /* Add the font size here */
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.8); /* Add the filter shadow here */
+`;
+
+const StyledExclamation = styled.span`
+  color: #eeb527; /* r */
+  font-size: 50px; /* Add the font size here */
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.8); /* Add the filter shadow here */
 `;
 
 const About = () => {
@@ -142,12 +226,25 @@ const About = () => {
           </ImageWrapper>
           <br />
           <StyledSalute>
-            Gracias por elegir Mexplorer. <br />
+            Gracias por elegirnos. <br />
             Te invitamos a explorar nuestro sitio web, navegar por nuestras
             ofertas y comenzar a planificar tus vacaciones de ensueño en Cancún.
             Permítenos ayudarte a crear una experiencia que te dejará recuerdos
-            para toda la vida. ¡Prepárate para descubrir la magia de Cancún con
-            Mexplorer!
+            para toda la vida. <br />
+            ¡Prepárate para descubrir la magia de Cancún con
+            <br />
+            <br />
+            <br />
+            <StyledMexplorer>M</StyledMexplorer>
+            <StyledE>e</StyledE>
+            <StyledX>x</StyledX>
+            <StyledP>p</StyledP>
+            <StyledL>l</StyledL>
+            <StyledO>o</StyledO>
+            <StyledR1>r</StyledR1>
+            <StyledE>e</StyledE>
+            <StyledR2>r</StyledR2>
+            <StyledExclamation>!</StyledExclamation>
           </StyledSalute>
         </Content>
       </Wrapper>
