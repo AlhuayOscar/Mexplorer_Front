8fb764c7595d5e828465f429ab1a0754e9da7f00
John Jaramillo <jaramillor03@gmail.com>
2023-08-02 16:05:23 -0500

    prueba numero 1

--- pages/tour/[id].js
+++ pages/tour/[id].js
@@ -2,7 +2,6 @@ import Center from "@/components/Center";
 import Header from "@/components/Header";
 import { mongooseConnect } from "@/lib/mongoose";
 import { Tour } from "@/models/Tour";
-const { URL_BACK } = process.env;
 import styled, { css } from "styled-components";
 import { useContext, useState, useEffect, useRef } from "react";
 import { CartContext } from "@/components/CartContext";
@@ -19,17 +18,11 @@ import ToursReviews from "@/components/ToursReviews";
 import Footer from "@/components/Footer";
 import ToursGrid from "@/components/ToursGrid";
 import NavTour from "@/components/NavTour";
-import {
-  CancelPresentationOutlined,
-  Diversity1Sharp,
-  Reviews,
-} from "@mui/icons-material";
-import FacebookIcon from "@mui/icons-material/Facebook";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import TelegramIcon from "@mui/icons-material/Telegram";
 import ReviewBox from "@/components/ReviewBox";
 import TimeBox from "@/components/TimeBox";
-import axios from "axios";
+
 
 const ColWrapper = styled.div`
   display: flex;
@@ -300,278 +293,14 @@ const StyledWhatsapp = getStyledComponent(WhatsAppIcon);
 const StyledTwitter = getStyledComponent(TwitterIcon);
 const StyledTelegram = getStyledComponent(TelegramIcon);
 
-export default function TourPage({ tour, promoTours }) {
+export default function TourPage({ tour, conection }) {
   console.log(tour);
-  const { addTour } = useContext(CartContext);
-  const [showDescription, setShowDescription] = useState(true);
-  const [showIncludes, setShowIncludes] = useState(false);
-  const [showRequirements, setShowRequirements] = useState(false);
-  const [showNotes, setShowNotes] = useState(false);
-  const includesRef = useRef(null);
-  const requirementsRef = useRef(null);
-  const notesRef = useRef(null);
-  const reviewsRef = useRef(null);
-  const recomendationsRef = useRef(null);
-  const reservationsRef = useRef(null);
-  const [currentUrl, setCurrentUrl] = useState("");
-  const [showOverlay, setShowOverlay] = useState(false);
-
-  useEffect(() => {
-    // Get the current URL only when the component mounts (client-side).
-    setCurrentUrl(window.location.href);
-  }, []);
-
-  const handleShowDescription = () => {
-    setShowDescription(!showDescription);
-  };
-
-  const handleShowIncludes = () => {
-    setShowIncludes(!showIncludes);
-  };
-
-  const handleShowRequirements = () => {
-    setShowRequirements(!showRequirements);
-  };
-
-  const handleShowNotes = () => {
-    setShowNotes(!showNotes);
-  };
-
-  const scrollToreservations = () => {
-    reservationsRef.current.scrollIntoView({ behavior: "smooth" });
-  };
-
-  useEffect(() => {
-    const handleResize = () => {
-      setShowOverlay(window.innerWidth < 768);
-    };
-
-    handleResize(); // Verificamos el tamaño al cargar el componente
-
-    window.addEventListener("resize", handleResize); // Agregamos el evento para cambios en el tamaño de la pantalla
-
-    return () => {
-      window.removeEventListener("resize", handleResize); // Eliminamos el evento al desmontar el componente
-    };
-  }, []);
-
+  console.log(conection)
+  
+  
   return (
     <>
-      <Header />
-      <Overlay show={showOverlay} />
-      <OverflowProtection>
-        <ToursImageCarousel images={tour.images} />
-      </OverflowProtection>
-      <TitleTour>
-        <div>
-          <Title>{tour.name}</Title>
-          <TimeBox duration={tour.duration} /* white */ />
-        </div>
-        <div>
-          <ReviewBox review={tour.review} />
-          <Link
-            href={`https://api.whatsapp.com/send?text=${encodeURIComponent(
-              currentUrl
-            )}`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <StyledWhatsapp />
-          </Link>
-          <Link
-            href={`https://twitter.com/intent/tweet?url=${encodeURIComponent(
-              currentUrl
-            )}&text=${encodeURIComponent("Echa un vistazo a este tour!")}`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <StyledTwitter />
-          </Link>
-          <Link
-            href={`https://t.me/share/url?url=${encodeURIComponent(
-              currentUrl
-            )}&text=${encodeURIComponent("Echa un vistazo a este tour!")}`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <StyledTelegram />
-          </Link>
-        </div>
-      </TitleTour>
-      <NavTour
-        includesRef={includesRef}
-        requirementsRef={requirementsRef}
-        notesRef={notesRef}
-        reviewsRef={reviewsRef}
-        recomendationsRef={recomendationsRef}
-      />
-      <ReservationBtn onClick={scrollToreservations}>
-        Reserva ahora!!!
-      </ReservationBtn>
-      <MovilHeader>
-        <HeaderInfo pink>
-          Descripción general
-          <div onClick={handleShowDescription}>
-            {showDescription ? <ExpandLessIcon /> : <ExpandMoreIcon />}
-          </div>
-        </HeaderInfo>
-        {showDescription && <Description>{tour.description}</Description>}
-        <HeaderInfo yellow>
-          Que incluye
-          <div onClick={handleShowIncludes}>
-            {showIncludes ? <ExpandLessIcon /> : <ExpandMoreIcon />}
-          </div>
-        </HeaderInfo>
-        {showIncludes && (
-          <InfoBox>
-            <Subtitle yellow>Este tour incluye:</Subtitle>
-            <Points>
-              {tour.includes?.map((include) => (
-                <Point key={include}>
-                  <Check />
-                  {include}
-                </Point>
-              ))}
-              <Subtitle yellow>Este tour no incluye:</Subtitle>
-              {tour.doesntIncludes?.map((doesntInclude) => (
-                <Point key={doesntInclude}>
-                  <Cancel />
-                  {doesntInclude}
-                </Point>
-              ))}
-            </Points>
-          </InfoBox>
-        )}
-        <HeaderInfo purple>
-          Que llevar
-          <div onClick={handleShowRequirements}>
-            {showRequirements ? <ExpandLessIcon /> : <ExpandMoreIcon />}
-          </div>
-        </HeaderInfo>
-        {showRequirements && (
-          <InfoBox>
-            <Subtitle purple>A este tour recomendamos llevar:</Subtitle>
-            <Points>
-              {tour.requirements?.map((requirement) => (
-                <Point key={requirement}>
-                  <Check />
-                  {requirement}
-                </Point>
-              ))}
-            </Points>
-          </InfoBox>
-        )}
-        <HeaderInfo green>
-          Notas
-          <div onClick={handleShowNotes}>
-            {showNotes ? <ExpandLessIcon /> : <ExpandMoreIcon />}
-          </div>
-        </HeaderInfo>
-        {showNotes && (
-          <InfoBox>
-            <Subtitle green>Notas y recomendaciones:</Subtitle>
-            <Points>
-              {tour.notes?.map((note) => (
-                <Point key={note}>
-                  <Check />
-                  {note}
-                </Point>
-              ))}
-            </Points>
-          </InfoBox>
-        )}
-        <Center>
-          <Reservation tour={tour} reservationsRef={reservationsRef} />
-        </Center>
-      </MovilHeader>
-      <Desktop>
-        <ColWrapper>
-          <TourInfoBox>
-            <Subtitle title>Descripción general</Subtitle>
-            <ToursLink href={"/tours"}>
-              <div>
-                <ArrowI />
-                Ver todos los tours en Cancun
-              </div>
-            </ToursLink>
-            <Description>{tour.description}</Description>
-
-            {tour.includes && (
-              <InfoBox ref={includesRef}>
-                <Subtitle yellow>Que incluye</Subtitle>
-                <Points short>
-                  <h4>Este tour incluye:</h4>
-                  {tour.includes?.map((include) => (
-                    <Point key={include}>
-                      <Check />
-                      {include}
-                    </Point>
-                  ))}
-                </Points>
-                {tour.doesntIncludes && (
-                  <>
-                    <Points short>
-                      <h4>Este tour no incluye:</h4>
-                      {tour.doesntIncludes?.map((doesntInclude) => (
-                        <Point key={doesntInclude}>
-                          <Cancel />
-                          {doesntInclude}
-                        </Point>
-                      ))}
-                    </Points>
-                  </>
-                )}
-              </InfoBox>
-            )}
-            {tour.requirements && (
-              <InfoBox ref={requirementsRef}>
-                <Subtitle purple>Que Llevar</Subtitle>
-                <Points long>
-                  {tour.requirements?.map((requirement) => (
-                    <Point key={requirement}>
-                      <Check />
-                      {requirement}
-                    </Point>
-                  ))}
-                </Points>
-              </InfoBox>
-            )}
-
-            {tour.notes && (
-              <InfoBox ref={notesRef}>
-                <Subtitle green>Notas</Subtitle>
-                <Points long>
-                  {tour.notes?.map((note) => (
-                    <Point key={note}>
-                      <Check />
-                      {note}
-                    </Point>
-                  ))}
-                </Points>
-              </InfoBox>
-            )}
-            {/* <div>
-                <button onClick={() => addTour(tour._id)}>
-                  Añadir al carrito
-                </button>
-            </div> */}
-          </TourInfoBox>
-          <Reservation tour={tour} sticky={true} />
-        </ColWrapper>
-      </Desktop>
-      <Center>
-        <Subtitle red margin ref={reviewsRef}>
-          Reseñas
-        </Subtitle>
-        <ToursReviews tour={tour} />
-      </Center>
-      <Recomendations>
-        <Subtitle purple margin ref={recomendationsRef}>
-          Recomendaciones
-        </Subtitle>
-        <ToursGrid tours={promoTours} />
-      </Recomendations>
-      <Footer />
+      <p> estoy en iddddddddddddddddd wachoooooooooooo</p>
     </>
   );
 }
@@ -589,21 +318,29 @@ export default function TourPage({ tour, promoTours }) {
   };
 } */
 
-const tourCache = new Map();
+
 
 export async function getServerSideProps(context) {
-  await mongooseConnect();
-  const { id } = context.query;
-  const tour = await Tour.findById(id);
-  tourCache.set(id, tour);
+  
 
-  const promoTours = await Tour.find({ promo: true }, null, { limit: 3 });
+let conection;
+try {
+  await mongooseConnect()
+  conection = 'conecto'
+} catch (error) {
+  conection = `no conectoooo ${error}`
+}
+ 
+  
+  const { id } = context.query;
+  
+  console.log(id)
 
   return {
     props: {
-      tour: JSON.parse(JSON.stringify(tour)),
-      promoTours: JSON.parse(JSON.stringify(promoTours)),
-    },
-  };
+      tour: id,
+      conection
+    }
+  }
 }
 
