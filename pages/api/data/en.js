8d65cc05ec73ef619e08b45f139f936ef0a0396c
Alan Winkler <alanquimeywinkler@gmail.com>
2023-08-03 15:11:29 -0300

    ahora con try-catch

--- pages/api/data/en.js
+++ pages/api/data/en.js
@@ -5,18 +5,21 @@ export default async function handle(req, res) {
   const { method } = req;
   await mongooseConnect();
   const { _id } = req.query;
-
-  if (method === "GET") {
-    if (_id) {
-      const tour = await Tour.findOne({ _id }).select(
-        "nameEng subtitleEng descriptionEng includesEng requirementsEng doesntIncludesEng notesEng"
-      );
-      return res.status(200).json(tour);
-    } else {
-      const tours = await Tour.find({}).select(
-        "nameEng subtitleEng descriptionEng includesEng requirementsEng doesntIncludesEng notesEng"
-      );
-      return res.status(200).json(tours);
+  try {
+    if (method === "GET") {
+      if (_id) {
+        const tour = await Tour.findOne({ _id }).select(
+          "nameEng subtitleEng descriptionEng includesEng requirementsEng doesntIncludesEng notesEng"
+        );
+        return res.status(200).json(tour);
+      } else {
+        const tours = await Tour.find({}).select(
+          "nameEng subtitleEng descriptionEng includesEng requirementsEng doesntIncludesEng notesEng"
+        );
+        return res.status(200).json(tours);
+      }
     }
+  } catch (error) {
+    return res.status(400).json({ msg: error.message });
   }
 }
