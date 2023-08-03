324f9148c7a6fd6fee034d9549d85b29deb0aa9e
Alan Winkler <alanquimeywinkler@gmail.com>
2023-08-03 18:00:23 -0300

    agregué rutas inglés y español de blogs

--- models/Blog.js
+++ models/Blog.js
@@ -2,8 +2,11 @@ import { model, models, Schema } from "mongoose";
 
 const BlogSchema = new Schema({
   title: { type: String, required: true },
+  titleEng: { type: String, required: true },
   subtitle: { type: String },
+  subtitleEng: { type: String },
   description: { type: String },
+  descriptionEng: { type: String },
   images: { type: [String] },
   date: { type: Date },
   location: { type: String },
