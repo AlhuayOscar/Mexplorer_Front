a9cc51e3d6dccd3f5e1e17fb4e18a2729781473f
AlhuayOscar <90626121+AlhuayOscar@users.noreply.github.com>
2023-08-16 15:34:44 -0300

    Urls

--- components/FollowUs.js
+++ components/FollowUs.js
@@ -1,8 +1,5 @@
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import Link from "next/link";
-import WhatsAppIcon from "@mui/icons-material/WhatsApp";
-import TwitterIcon from "@mui/icons-material/Twitter";
-import TelegramIcon from "@mui/icons-material/Telegram";
 import { useTranslation } from "react-i18next";
 
 const CenteredContainer = styled.div`
@@ -12,17 +9,17 @@ const CenteredContainer = styled.div`
   display: flex;
   align-items: center;
 
-  p{
+  p {
     font-size: 1.6rem;
     font-weight: bold;
     color: #84c441;
   }
 
-  div{
+  div {
     display: flex;
     gap: 15px;
   }
-  `;
+`;
 
 const Icon = styled.img`
   width: 25px;
@@ -30,38 +27,90 @@ const Icon = styled.img`
   margin-inline: 5px;
 `;
 
-const LinkStyled = styled(Link)`
+const LinkStyled = styled.a`
   display: flex;
   place-items: center;
   background-color: #84c441;
   padding: 10px;
   border-radius: 25%;
+  text-decoration: none;
+  color: inherit;
   &:hover {
     background-color: #699c34;
     scale: 1.02;
     transition: 0.5s ease-in-out;
   }
 `;
-const FollowUs = ({ socialUrls }) => {
+
+const FollowUs = () => {
   const { t } = useTranslation();
+  const [socialUrls, setSocialUrls] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("/api/urls")
+      .then((response) => response.json())
+      .then((data) => {
+        if (!data || data.length === 0) {
+          console.error("No se encontraron URLs de Portada.");
+          setLoading(false);
+          return;
+        }
+
+        // Get the index of the target URLs in the data array
+        const whatsappIndex = data[0].urlName.findIndex(
+          (item) => item === "Whatsapp"
+        );
+        const facebookIndex = data[0].urlName.findIndex(
+          (item) => item === "Facebook"
+        );
+        const instagramIndex = data[0].urlName.findIndex(
+          (item) => item === "Instagram"
+        );
+        const tripadvisorIndex = data[0].urlName.findIndex(
+          (item) => item === "Trip"
+        );
+
+        setSocialUrls({
+          whatsapp:
+            whatsappIndex !== -1 ? data[0].videoUrls[whatsappIndex] : "",
+          facebook:
+            facebookIndex !== -1 ? data[0].videoUrls[facebookIndex] : "",
+          instagram:
+            instagramIndex !== -1 ? data[0].videoUrls[instagramIndex] : "",
+          tripadvisor:
+            tripadvisorIndex !== -1 ? data[0].videoUrls[tripadvisorIndex] : "",
+        });
+
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error al obtener las URLs de Portada:", error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <CenteredContainer>
       <p>{t("Síguenos")}:</p>
       <div>
-        <LinkStyled href={'https://mexplorer-front-three.vercel.app/'}>
+        <LinkStyled href={socialUrls.facebook}>
           <Icon
             src="https://res.cloudinary.com/dipn8zmq3/image/upload/v1689651875/facy_toglrc.png"
             alt="Facebook"
           />
         </LinkStyled>
-        <LinkStyled href={'https://mexplorer-front-three.vercel.app/'}>
+        <LinkStyled href={socialUrls.instagram}>
           <Icon
             src="https://res.cloudinary.com/dipn8zmq3/image/upload/v1689651875/insy_lcdcik.png"
             alt="Instagram"
           />
         </LinkStyled>
-        <LinkStyled href={'https://mexplorer-front-three.vercel.app/'}>
+        <LinkStyled href={socialUrls.tripadvisor}>
           <Icon
             src="https://res.cloudinary.com/dipn8zmq3/image/upload/v1689651875/trippy_gpvp7q.png"
             alt="TripAdvisor"
